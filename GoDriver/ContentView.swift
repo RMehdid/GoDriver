f9ebe81a1096613e0003a412a63e79e1dbe0d1d7
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2024-02-29 12:14:01 +0100

    Initial Commit

---
+++ GoDriver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoDriver
+//
+//  Created by Samy Mehdid on 29/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoDriver/GoDriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoDriverApp.swift
+//  GoDriver
+//
+//  Created by Samy Mehdid on 29/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct GoDriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
